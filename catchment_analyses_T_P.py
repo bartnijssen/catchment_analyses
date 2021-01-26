9c7be4e9969f6f9ec49ff6392164a6fa76397c87
Bart Nijssen <bart.nijssen@gmail.com>
2021-01-26 11:41:02 -0800

    Weighted averaging of T and P

--- catchment_analyses_T_P.py
+++ catchment_analyses_T_P.py
@@ -8,10 +8,8 @@ import numpy as np
 gcm_list = ['MIROC5', 'CanESM2', 'CCSM4', 'CNRM-CM5', 'CSIRO-Mk3-6-0',
             'GFDL-ESM2M', 'HadGEM2-CC', 'HadGEM2-ES', 'inmcm4', 'IPSL-CM5A-MR']
 scenario_list = ['RCP45', 'RCP85']
-hydro_model_list = [('VIC', 'calib_inverse', 'P1'),
-                    ('VIC', 'ORNL', 'P2'),
-                    ('VIC', 'NCAR', 'P3'),
-                    ('PRMS', 'calib_inverse', 'P1')]
+# NCAR files contain precipitation and temperature
+hydro_model_list = [('VIC', 'NCAR', 'P3')]
 downscaling = 'maca'
 locations = pd.read_csv(
     './rupp_et_al_2020_locations.csv', header=None)[0].values
@@ -43,23 +41,23 @@ for (hydro_model, path_parameter_name, official_convention) in hydro_model_list:
             try:
                 print('Processing data file: {}'.format(file_path))
                 data = xr.open_dataset(file_path)
-                data = data[["SWE"]].load().transpose("lat", "lon", "time").compute()
-                swe_attrs = data['SWE'].attrs
+                data = data[['Precipitation', 'Tair']].load()
+                prcp_attrs = data['Precipitation'].attrs
+                tair_attrs = data['Tair'].attrs
             except:
                 print('Failed to open data file: {}'.format(file_path))
 
             for location_id in locations:
-                out_path = '/pool0/data/nijssen/crcc/rupp_et_al_2020/catchment_{location_id}_weighted_mean_19500101-20991231_{scenario}_{gcm}_{downscaling}_{hydro_model}-{official_convention}.nc'.format(
+                out_path = '/pool0/data/nijssen/crcc/rupp_et_al_2020/catchment_{location_id}_weighted_mean_T_P_19500101-20991231_{scenario}_{gcm}_{downscaling}.nc'.format(
                     location_id=location_id,
-                    hydro_model=hydro_model,
                     downscaling=downscaling,
                     scenario=scenario,
-                    official_convention=official_convention,
                     gcm=gcm)
                 try:
                     print('\tProcessing location {}'.format(location_id))
                     data_subset = data.where(mask[location_id]).weighted(weight[location_id]).mean(dim=['lat', 'lon'], keep_attrs=True)
-                    data_subset['SWE'].attrs = swe_attrs
+                    data_subset = 'Precipitation'].attrs = prcp_attrs
+                    data_subset = 'Tair'].attrs = tair_attrs
                     data_subset.to_netcdf(out_path)
                 except:
                     print('Oh no! Something failed for {} for location {}'.format(
